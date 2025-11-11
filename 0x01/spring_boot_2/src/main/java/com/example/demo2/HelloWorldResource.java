4d493d313f3e010ebe099f95aab5e40d3d83e3df
Felipe Anias <felipeanias@hotmail.com>
2025-11-11 16:51:15 -0300

    Exercicio 1

---
+++ 0x01/spring_boot_2/src/main/java/com/example/demo2/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
+public class HelloWorldResource {
+	
+	private static final String MESSAGE = "PROJETO MAVEN (SPRING INITIALIZR) CRIADO COM SUCESSO !!!";
+	
+	@GetMapping("/home")
+	public String home() {
+		return MESSAGE;
+	}
+}
